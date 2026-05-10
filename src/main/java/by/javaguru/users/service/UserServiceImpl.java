a701b5a9761dbc8f060eb40515b798731becdb08
timmaxx <m.n.timchenko@gmail.com>
2026-05-10 17:07:00 +0300

    Lesson 1.9. ErrorHandling (first exception handler on DuplicateKeyException)

--- src/main/java/by/javaguru/users/service/UserServiceImpl.java
+++ src/main/java/by/javaguru/users/service/UserServiceImpl.java
@@ -4,9 +4,12 @@ import by.javaguru.users.data.UserRepository;
 import by.javaguru.users.service.dto.CreateUserDto;
 import by.javaguru.users.service.dto.UserDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -25,7 +28,9 @@ public class UserServiceImpl implements UserService {
         return createUserDtoMono
                 .mapNotNull(userMapper::createUserDtoToUserEntity)
                 .flatMap(userRepository::save)
-                .mapNotNull(userMapper::userEntityToUserDto);
+                .mapNotNull(userMapper::userEntityToUserDto)
+                .onErrorMap(DuplicateKeyException.class,
+                        e -> new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage()));
     }
 
     @Override
