e1bde7e816c118eb78338136d4848a78314a68df
timmaxx <m.n.timchenko@gmail.com>
2026-05-20 22:30:18 +0300

    Lesson 2.10 (add exception to the method createUser (POST) so that it can be executed without authorisation)

---
+++ src/main/java/by/javaguru/users/config/WebSecurity.java
@@ -0,0 +1,25 @@
+package by.javaguru.users.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+
+@Configuration
+@EnableWebFluxSecurity
+public class WebSecurity {
+
+    @Bean
+    SecurityWebFilterChain httpSecurityFilterChain(ServerHttpSecurity http) {
+        return http.authorizeExchange(
+                        exchange -> exchange.pathMatchers(HttpMethod.POST, "/users")
+                                .permitAll()
+                                .anyExchange().authenticated())
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .build();
+    }
+
+}
